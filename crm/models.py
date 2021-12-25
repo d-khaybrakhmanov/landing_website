7389cd6ba001c35b3336457a3a9b372b86099478
Dmitry <mityai.moto@gmail.com>
2021-12-26 02:20:53 +0300

    first_comit

--- crm/models.py
+++ crm/models.py
@@ -12,7 +12,7 @@ class StatusCrm(models.Model):
         verbose_name_plural = 'Статусы'
 
 class Order(models.Model):
-    order_dt = models.DateTimeField(auto_now=True)
+    order_dt = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
     order_name = models.CharField(max_length=200, verbose_name='Имя')
     order_phone = models.CharField(max_length=200, verbose_name= 'Телефон')
     order_status = models.ForeignKey(StatusCrm, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Статус')
